4f2dcb8291ef34fb3821095fa6a25d813a0ee3fd
kefan43 <k27dong@edu.uwaterloo.ca>
2019-02-26 22:58:27 -0500

    Site updated: 2019-02-26 22:58:24

--- dist/music.js
+++ dist/music.js
@@ -7,7 +7,7 @@ const ap = new APlayer({
         name: "主义第一",
         artist: '窦唯',
         url: 'https://www.youtube.com/watch?v=cfzSG5zgsEY',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        cover: '/images/music_pics/tzjg.jepg',
       }
     ]
 });
